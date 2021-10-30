39d3d2e6bddc068e0eb5b6859a88c2f24910740f
soregashi-27 <tomo.soul.15@gmail.com>
2021-10-31 01:46:59 +0900

    nginxのmoduleを作成

--- envs/prod/app/recordable/ecr.tf
+++ envs/prod/app/recordable/ecr.tf
@@ -1,30 +1,5 @@
-resource "aws_ecr_repository" "nginx" {
+module "nginx" {
+  source = "../../../../modules/ecr"
+
   name = "infra-prod-recordable-nginx"
-
-  tags = {
-    Name = "infra-prod-recordable-nginx"
-  }
-}
-
-resource "aws_ecr_lifecycle_policy" "nginx" {
-  policy = jsonencode(
-    {
-      "rules" : [
-        {
-          "rulePriority" : 1,
-          "description" : "Hold only 10 images, expire all others",
-          "selection" : {
-            "tagStatus" : "any",
-            "countType" : "imageCountMoreThan",
-            "countNumber" : 10
-          },
-          "action" : {
-            "type" : "expire"
-          }
-        }
-      ]
-    }
-  )
-
-  repository = aws_ecr_repository.nginx.name
 }
