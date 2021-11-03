5c01594d18ea8251ba2d449dbcec6a9048069015
soregashi-27 <tomo.soul.15@gmail.com>
2021-11-03 21:28:29 +0900

    Availability zoneのpublic/private subnetを追加

--- envs/prod/network/main/variables.tf
+++ envs/prod/network/main/variables.tf
@@ -2,3 +2,21 @@ variable "vpc_cdir" {
   type    = string
   default = "172.31.0.0/16"
 }
+
+variable "azs" {
+  type = map(object({
+    public_cidr  = string
+    private_cidr = string
+  }))
+
+  default = {
+    a = {
+      public_cidr  = "172.31.0.0/20"
+      private_cidr = "172.31.48.0/20"
+    },
+    c = {
+      public_cidr  = "172.31.16.0/20"
+      private_cidr = "172.31.64.0/20"
+    }
+  }
+}
