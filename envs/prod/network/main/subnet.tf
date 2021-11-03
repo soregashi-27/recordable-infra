7bfd6098157d7560f25a29c8fd96067238cceb06
soregashi-27 <tomo.soul.15@gmail.com>
2021-11-03 22:50:22 +0900

    private subnetを追加

--- envs/prod/network/main/subnet.tf
+++ envs/prod/network/main/subnet.tf
@@ -14,12 +14,12 @@ resource "aws_subnet" "public" {
 resource "aws_subnet" "private" {
   for_each = var.azs
 
-  availability_zone = "${data.aes_region.current.name}${each.key}"
+  availability_zone = "${data.aws_region.current.name}${each.key}"
   cidr_block = each.value.private_cidr
   map_public_ip_on_launch = false
   vpc_id = aws_vpc.this.id
 
   tags = {
-    Name = "${aws_vpc.this.tags.Name}-public-${each.key}"
+    Name = "${aws_vpc.this.tags.Name}-private-${each.key}"
   }
 }
