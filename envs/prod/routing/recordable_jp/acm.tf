ac741559cae9e1b31041ad3406602810622228e9
soregashi-27 <tomo.soul.15@gmail.com>
2021-11-07 09:06:43 +0900

    ACMの発行成功

--- envs/prod/routing/recordable_jp/acm.tf
+++ envs/prod/routing/recordable_jp/acm.tf
@@ -11,3 +11,7 @@ resource "aws_acm_certificate" "root" {
     create_before_destroy = true
   }
 }
+
+resource "aws_acm_certificate_validation" "root" {
+  certificate_arn = aws_acm_certificate.root.arn
+}
